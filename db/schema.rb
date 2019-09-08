c2df0bd813a040db792a5f5174f14bd9871523b1
Vincent <vincethewalker@yahoo.co.uk>
2019-09-08 14:46:10 +0100

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180425154316) do
+ActiveRecord::Schema.define(version: 2018_04_25_154316) do
 
   create_table "todos", force: :cascade do |t|
     t.string "name"
-    t.text   "description"
+    t.text "description"
   end
 
 end
